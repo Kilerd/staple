9b393644324c252b3c8d34e790137a7d43154a4b
Kilerd <blove694@gmail.com>
2020-09-05 15:36:48 +0800

    feat: implement list command.

--- src/command/list.rs
+++ src/command/list.rs
@@ -1,9 +1,15 @@
 use crate::{app::App, error::StapleError};
+use colored::*;
 
 pub(crate) fn command() -> Result<(), StapleError> {
-    let result = App::load(false)?;
+    let app = App::load(false)?;
+    info!("Project Name: {}", app.config.site.title);
+    let mut pages = app.load_all_data()?;
+    pages.reverse();
+    for page in pages {
+        let draw = if page.draw { "DRAW" } else { "" };
 
-    info!("Project Name: {}", result.config.site.title);
-
+        info!("{} {:4} {}({})", page.datetime.format("%b %d, %Y"), draw.blue(), page.title.white().magenta(), page.url);
+    }
     Ok(())
 }
