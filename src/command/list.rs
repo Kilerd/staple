b9463bb413566f103754d6e0068deaf25d4233ab
Kilerd <blove694@gmail.com>
2020-09-05 15:39:22 +0800

    refact: format code.

--- src/command/list.rs
+++ src/command/list.rs
@@ -9,7 +9,13 @@ pub(crate) fn command() -> Result<(), StapleError> {
     for page in pages {
         let draw = if page.draw { "DRAW" } else { "" };
 
-        info!("{} {:4} {}({})", page.datetime.format("%b %d, %Y"), draw.blue(), page.title.white().magenta(), page.url);
+        info!(
+            "{} {:4} {}({})",
+            page.datetime.format("%b %d, %Y"),
+            draw.blue(),
+            page.title.white().magenta(),
+            page.url
+        );
     }
     Ok(())
 }
